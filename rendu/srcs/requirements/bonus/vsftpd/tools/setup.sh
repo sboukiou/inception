378bda4d2109ac743f4447f3049168c9fba3443b
Said <sboukiou@student.1337.ma>
2026-07-02 15:22:35 +0100

    Removed user creation, user already exists

--- rendu/srcs/requirements/bonus/vsftpd/tools/setup.sh
+++ rendu/srcs/requirements/bonus/vsftpd/tools/setup.sh
@@ -5,11 +5,10 @@ FTP_PASS=$(cat /run/secrets/ftp_password)
 
 if ! id -u "$FTP_USER" >/dev/null 2>&1; then
     echo "Creating FTP user: $FTP_USER"
-    adduser --disabled-password --home /var/www/wordpress --gecos "" "$FTP_USER"
+    adduser --disabled-password  --gecos "" "$FTP_USER"
     echo "$FTP_USER:$FTP_PASS" | chpasswd
 fi
 
 chown -R "$FTP_USER:$FTP_USER" /var/www/wordpress
 
-echo "Starting vsftpd..."
 exec "$@"
