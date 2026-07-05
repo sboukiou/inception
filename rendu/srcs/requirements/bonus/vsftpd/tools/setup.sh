e060d890d1d19a5adcb0d4ef3cdd94561d8a75bd
Said <sboukiou@student.1337.ma>
2026-07-05 00:27:34 +0000

    final edit check on the vsftpd container setup script

--- rendu/srcs/requirements/bonus/vsftpd/tools/setup.sh
+++ rendu/srcs/requirements/bonus/vsftpd/tools/setup.sh
@@ -5,7 +5,7 @@ FTP_PASS=$(cat /run/secrets/ftp_password)
 
 if ! id -u "$FTP_USER" >/dev/null 2>&1; then
     echo "Creating FTP user: $FTP_USER"
-    adduser --disabled-password  --gecos "" "$FTP_USER"
+    adduser --disabled-password --gecos "" "$FTP_USER"
     echo "$FTP_USER:$FTP_PASS" | chpasswd
 fi
 
